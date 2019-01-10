93bfc82bb09775ac4524be2ec0adc5f0699cce93
Serge Vartanov <vartanov.s@gmail.com>
2019-01-10 15:30:32 -0800

    Added Max Line Length Rule and Server Status Codes

--- src/server/index.ts
+++ src/server/index.ts
@@ -9,8 +9,13 @@ const PORT = 3000;
 
 // Generate and Configure Server
 const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse): void => {
-  if (req.url === '/') res.write('Hello World from TS Node!');
-  else res.write('Route Not Found!');
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('Hello World from TS Node!');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.write('Route Not Found!');
+  }
   return res.end();
 });
 
